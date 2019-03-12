b5be39038230662aa184e8244030a1495d364154
Alan Lau <alanlau97@gmail.com>
2019-03-12 17:38:15 -0400

    forget to save

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import './Contact.css';
 
+const contact = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default contact;
