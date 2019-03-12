d68a846a795c136871ebb9ba63c9b1344e13b01c
Alan Lau <alanlau97@gmail.com>
2019-03-12 17:52:56 -0400

    modify contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Contact.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const contact = (props) => {
     return (
         <div>
-            
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">{props.FirstName}</h5>
+                            <p className="card-text">
+                                Last Name: {props.LastName} <br/>
+                                Birthday: {props.Birthday} <br/>
+                                Telephone: {props.Telephone}
+                            </p>
+                            {/*<a href="#" class="btn btn-primary">Go somewhere</a> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
